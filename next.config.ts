b500d79242dbe8edc9171266a9edcce1737c361d
Seungbeen Wi <wjb127@naver.com>
2025-03-29 15:30:27 +0900

    Remove ESLint Check

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // 빌드 시 ESLint 검사 비활성화
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
